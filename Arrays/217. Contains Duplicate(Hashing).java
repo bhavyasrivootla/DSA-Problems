e03916ea56270b9e9ed2212125845940566a99e9
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-06-20 22:48:24 +0530

    Implement containsDuplicate method using HashSet

---
+++ Arrays/217. Contains Duplicate(Hashing).java
@@ -0,0 +1,23 @@
+/*Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.*/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
