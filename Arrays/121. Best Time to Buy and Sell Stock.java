bacfb9789d64504317ae4bb552d1030f593e9c9c
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-06-28 23:30:08 +0530

    Revise problem description for stock profit calculation
    Updated problem statement to reflect stock buying and selling scenario.

--- Arrays/121. Best Time to Buy and Sell Stock.java
+++ Arrays/121. Best Time to Buy and Sell Stock.java
@@ -1,13 +1,18 @@
 /*
-Given an integer array nums, find the subarray with the largest sum, and return its sum.
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
  
 
 Example 1:
 
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
   */
 
 /* Brute force Approach
