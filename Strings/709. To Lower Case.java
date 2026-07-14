d129a850eeeda4c039582a51d936c0dc7ba27640
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-07-15 00:11:51 +0530

    Create 709. To Lower Case.java

---
+++ Strings/709. To Lower Case.java
@@ -0,0 +1,18 @@
+/*
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+  */
+
+class Solution {
+    public String toLowerCase(String s) {
+        String ans = s.toLowerCase();
+        return ans;
+    }
+    
+}
