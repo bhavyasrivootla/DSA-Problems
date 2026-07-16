b2c21f5f6baca8377656ca25b856ea765c6a44c3
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-07-16 11:59:42 +0530

    Add solution for Valid Anagram problem
    Implement a method to check if one string is an anagram of another.

---
+++ Strings/242. Valid Anagram.java
@@ -0,0 +1,28 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+
+Output: true
+  */
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character,Integer> map1 = new HashMap<>();
+        HashMap<Character,Integer> map2 = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
+        }
+        for(char ch : t.toCharArray()){
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+        }
+        if(map1.equals(map2)){
+            return true;
+        }
+        return false;
+    }
+}
