cb51160e8d3b7f0c7fcae76a37ffc5b40797c368
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-07-17 14:23:58 +0530

    Sliding Window Approach - Fixed Window
    Implemented a solution to find the maximum average of a contiguous subarray of length k.

---
+++ Arrays/643. Maximum Average Subarray I.java
@@ -0,0 +1,33 @@
+/*
+You are given an integer array nums consisting of n elements, and an integer k.
+
+Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,12,-5,-6,50,3], k = 4
+Output: 12.75000
+Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+  */
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        
+        int sum = 0;
+
+        for(int i = 0;i<k;i++){ //cal the first window sum 
+            sum+=nums[i];
+        }
+
+        int maxSum = sum;
+
+        for(int i = k;i<nums.length;i++){
+            sum -= nums[i-k]; //just remove the left num from the sum
+            sum += nums[i];//just add the right number to sum
+            maxSum = Math.max(sum,maxSum);
+        }
+        return (double) maxSum/k;
+    }
+}
