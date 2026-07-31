6aa97409616e9beac803b377aa1fb992a261131c
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-07-31 14:43:49 +0530

    Prefix Sum
    Implement a function to find the longest common prefix among an array of strings.

---
+++ Strings/14. Longest Common Prefix.java
@@ -0,0 +1,32 @@
+/*
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+*/
+
+public class Solution {
+    public static String longestCommonPrefix(String[] strs) {
+        String first = strs[0];
+        for(int i = 0;i<first.length();i++){
+            char ch = first.charAt(i);
+            for(int j = 1;j<strs.length;j++){
+                if(i == strs[j].length() || strs[j].charAt(i) != ch){
+                    return first.substring(0,i);
+                }
+            }
+        }
+        return first;
+    }
+}
