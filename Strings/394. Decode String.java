25215fdc327587d317866ef56830c3dd9938fed5
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-08-02 22:00:07 +0530

    Stack pattern

---
+++ Strings/394. Decode String.java
@@ -0,0 +1,54 @@
+/*
+Given an encoded string, return its decoded string.
+
+The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+
+You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+
+The test cases are generated so that the length of the output will never exceed 105.
+
+ 
+
+Example 1:
+
+Input: s = "3[a]2[bc]"
+Output: "aaabcbc"
+Example 2:
+
+Input: s = "3[a2[c]]"
+Output: "accaccacc"
+Example 3:
+
+Input: s = "2[abc]3[cd]ef"
+Output: "abcabccdcdcdef"
+  */
+
+class Solution {
+    public String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<Character> stringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        int currentNumber = 0;
+        for(char c : s.toCharArray()){
+            if(Character.isDigit(ch)){
+                currentNumber = currentNumber * 10 + (ch - '0');
+            }else if(ch == '['){
+                countStack.push(currentNumber);
+                stringStack.push(currentString);
+                
+                currentNumber = 0;
+                currentString = new StringBuilder();
+            }else if(ch == ']'){
+                int repeat = countStack.pop();
+                StringBuilder previous = stringStack.pop();
+
+                for(int i = 0;i<repeat;i++){
+                    previous.apppend(currentString);
+                }
+            }
+            else{ // If it is a character
+                sb.append(ch);
+            }
+        }
+    }
+}
