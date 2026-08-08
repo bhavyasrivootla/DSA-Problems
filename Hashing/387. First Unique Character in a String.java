ef605fe98df7995c1ae0b2d7ae5a88cd4a622ada
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-08-08 21:45:30 +0530

    Add solution for first unique character in a string
    Implement a solution to find the first unique character in a string.

---
+++ Hashing/387. First Unique Character in a String.java
@@ -0,0 +1,31 @@
+/*
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode"
+
+Output: 0
+
+Explanation:
+
+The character 'l' at index 0 is the first character that does not occur at any other index.
+  */
+
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        for(int i = 0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(map.get(ch)==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
