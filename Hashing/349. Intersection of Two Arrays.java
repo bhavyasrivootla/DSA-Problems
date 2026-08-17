83f2cfaef1fd7eca7cea08fa6930c6b5e5be63d8
bhavyasrivootla <vootlabhavyasri@gmail.com>
2026-08-17 13:01:29 +0530

    Add intersection method for two integer arrays
    Implement intersection method to find unique elements in both arrays.

---
+++ Hashing/349. Intersection of Two Arrays.java
@@ -0,0 +1,39 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+  */
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+        for(int x : nums1){
+            set.add(x); //Adding all the elements of nums1 into set
+        }
+        //loop through nums2 and check
+        for(int num : nums2){
+            if(set.contains(num)){
+                result.add(num);
+            }
+        }
+        //convert back the set into integer array
+        int[] ans = new int[result.size()];
+        int i = 0;
+        for(int x : result){
+            ans[i] = x;
+            i++;
+        }
+        return ans;
+    }
+}
